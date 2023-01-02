4ca04d1dd360f898bdde3bbdd43dec4a3b16aea3
Durgesh-2005 <112267270+Durgesh-2005@users.noreply.github.com>
2023-01-02 19:41:56 +0530

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+const navbar = document.querySelector('nav')
+const menu = navbar.querySelector('ul')
+const openbtn = navbar.querySelector('.fa-bars')
+const closebtn = menu.querySelector('.fa-times')
+
+openbtn.onclick = () => menu.classList.toggle('menu')
+closebtn.onclick = () => menu.classList.toggle('menu')
+
+
+
