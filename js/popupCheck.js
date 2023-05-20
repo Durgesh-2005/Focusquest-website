7ec3fcecba660edb4e5651a37d523df8b207c9ed
Durgesh <vvip202014@gmail.com>
2023-05-20 23:36:30 +0530

    POP UP RE VER

--- js/popupCheck.js
+++ js/popupCheck.js
@@ -1,22 +1,20 @@
+// popup.js
+
+let popupShown = false;
+
 // Check if the popup should be displayed
 function shouldDisplayPopup() {
-    // Check if the 'popupShown' cookie or 'popupShown' local storage item exists
-    return !document.cookie.includes('popupShown') && !localStorage.getItem('popupShown');
-  }
-  
-  // Show the popup
-  function showPopup() {
-    // Display the popup code here
-  
-    // Set the 'popupShown' cookie with an expiry date (e.g., 30 days)
-    document.cookie = 'popupShown=true; expires=' + new Date(Date.now() + 30 * 24 * 60 * 60 * 1000).toUTCString();
-  
-    // Set the 'popupShown' local storage item
-    localStorage.setItem('popupShown', 'true');
-  }
-  
-  // Check if the popup should be displayed and show it if necessary
-  if (shouldDisplayPopup()) {
-    showPopup();
-  }
-  
+  return !popupShown;
+}
+
+// Show the popup
+function showPopup() {
+  // Display the popup code here
+
+  popupShown = true;
+}
+
+// Check if the popup should be displayed and show it if necessary
+if (shouldDisplayPopup()) {
+  showPopup();
+}
